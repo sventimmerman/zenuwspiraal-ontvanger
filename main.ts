2f288e8c05823ca8ed3eb6cfea4a05f7068e695e
sventimmerman <100585338+sventimmerman@users.noreply.github.com>
2022-06-20 14:44:52 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber > 0) {
+        basic.showIcon(IconNames.Sad)
+        basic.showNumber(receivedNumber)
+        music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
+        basic.pause(1000)
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.No)
+        basic.showString("Game Over")
+        music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("Reset")
+    control.reset()
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "Game Over") {
+        basic.showIcon(IconNames.No)
+        basic.showString("Game Over")
+        music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
+    }
+})
+radio.setGroup(183)
+basic.showIcon(IconNames.Happy)
+music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
